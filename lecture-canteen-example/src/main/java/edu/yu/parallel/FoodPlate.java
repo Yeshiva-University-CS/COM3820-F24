84d02469335b20e6f960812a39bb86701517457d
sacknovi <sacknovitz@gmail.com>
2024-11-12 23:17:04 -0500

    missed file

---
+++ lecture-canteen-example/src/main/java/edu/yu/parallel/FoodPlate.java
@@ -0,0 +1,42 @@
+package edu.yu.parallel;
+
+public class FoodPlate {
+
+    private boolean isPizzaReady;
+    private boolean isBurgerReady;
+    private boolean isOtherJunkReady;
+    private String foodPlateCreatedBy;
+
+    public String getFoodPlateCreatedBy() {
+        return foodPlateCreatedBy;
+    }
+
+    public void setFoodPlateCreatedBy(String foodPlateCreatedBy) {
+        this.foodPlateCreatedBy = foodPlateCreatedBy;
+    }
+
+    public boolean isPizzaReady() {
+        return isPizzaReady;
+    }
+
+    public void setPizzaReady(boolean isPizzaReady) {
+        this.isPizzaReady = isPizzaReady;
+    }
+
+    public boolean isBurgerReady() {
+        return isBurgerReady;
+    }
+
+    public void setBurgerReady(boolean isBurgerReady) {
+        this.isBurgerReady = isBurgerReady;
+    }
+
+    public boolean isOtherJunkReady() {
+        return isOtherJunkReady;
+    }
+
+    public void setOtherJunkReady(boolean isOtherJunkReady) {
+        this.isOtherJunkReady = isOtherJunkReady;
+    }
+
+}
