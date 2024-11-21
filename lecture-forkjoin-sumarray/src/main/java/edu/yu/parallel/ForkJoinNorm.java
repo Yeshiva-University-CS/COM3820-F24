f553114ca83e44281cf3b3018f4071a9cf7281b4
sacknovi <sacknovitz@gmail.com>
2024-11-21 17:30:28 -0500

    Update ForkJoinNorm.java

--- lecture-forkjoin-sumarray/src/main/java/edu/yu/parallel/ForkJoinNorm.java
+++ lecture-forkjoin-sumarray/src/main/java/edu/yu/parallel/ForkJoinNorm.java
@@ -1,5 +1,8 @@
 package edu.yu.parallel;
 
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
 public class ForkJoinNorm extends AbstractArrayNorm {
     public static final int DEFAULT_THRESHOLD = 1000;
     private final int threshold;
@@ -24,6 +27,36 @@ public class ForkJoinNorm extends AbstractArrayNorm {
 
     @Override
     protected double computeNorm() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        ForkJoinPool pool = new ForkJoinPool();
+        double sumOfSquares = pool.invoke(new NormTask(0, array.length));
+        return Math.sqrt(sumOfSquares);
+    }
+
+    private class NormTask extends RecursiveTask<Double> {
+        private final int start;
+        private final int end;
+
+        public NormTask(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        protected Double compute() {
+            if (end - start < threshold) {
+                double sum = 0;
+                for (int i = start; i < end; i++) {
+                    sum += array[i] * array[i];
+                }
+                return sum;
+            } else {
+                int mid = start + (end - start) / 2;
+                NormTask left = new NormTask(start, mid);
+                NormTask right = new NormTask(mid, end);
+                left.fork();
+                right.fork();
+                return left.join() + right.join();
+            }
+        }
     }
 }
